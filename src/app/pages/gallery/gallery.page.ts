f1b96967772b7e47289d9dfd6e3bb641cdf79a2f
Unknown <secalero@ucm.es>
2021-07-05 19:59:16 +0200

    Gallery list of trollnets
    Retrieve list of trollnets, display, and prepare so that toggling the button activates or deactivates the trollnet against the backend
    Created trollnet-preview component

--- src/app/pages/gallery/gallery.page.ts
+++ src/app/pages/gallery/gallery.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// Services
+import { TrollnetStore } from '../../stores/trollnet.store';
+import { LoadingService } from '../../services/loading.service';
+
+// Models
+import { TrollnetModel } from '../../core/model/trollnet.model';
+
+import { filter, first,  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.page.html',
   styleUrls: ['./gallery.page.scss'],
 })
-export class GalleryPage implements OnInit {
+export class GalleryPage {
 
-  constructor() { }
+  /**
+   * Set of existing trollnets.
+   */
+  public retrievedTrollnets: TrollnetModel[];
 
-  ngOnInit() {
+    /**
+ * Trollnet gallery page constructor.
+   * @param trollnetStore Store for handling trollnets
+   * @param loadingService Service used to generate a loading dialog
+   */
+  constructor(private trollnetStore: TrollnetStore, private loadingService: LoadingService) {
+    this.retrievedTrollnets = [];
+    this.loadingService.show({
+      message: 'Loading trollnets...'
+    });
+    this.trollnetStore.trollnetsChange().subscribe(
+      (storeTrollnets: TrollnetModel[]) => {
+        this.retrievedTrollnets = storeTrollnets;
+      });
+    this.trollnetStore.trollnetsChange().pipe(
+      filter((array: TrollnetModel[]) => !!array.length),
+      first())
+    .subscribe(() => this.loadingService.dismiss());
   }
-
 }
